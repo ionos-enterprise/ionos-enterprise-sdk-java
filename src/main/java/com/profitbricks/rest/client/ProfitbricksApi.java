ecc20d092f00d9e6dc5e7e705515ffe42419989c
jasminSPC <jasmin@stackpointcloud.com>
2015-04-30 23:12:30 +0200

    Initial import

---
+++ src/main/java/com/profitbricks/rest/client/ProfitbricksApi.java
@@ -0,0 +1,13 @@
+package com.profitbricks.rest.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProfitbricksApi 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
