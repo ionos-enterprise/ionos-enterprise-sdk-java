bc58b3d6987142f65797e3aee277184fa25fe88d
jasminSPC <jasmin@stackpointcloud.com>
2015-05-21 03:20:13 +0200

    Code formatting

--- src/main/java/com/profitbricks/rest/client/RequestInterceptor.java
+++ src/main/java/com/profitbricks/rest/client/RequestInterceptor.java
@@ -4,7 +4,7 @@ import org.apache.http.client.methods.HttpRequestBase;
 
 public class RequestInterceptor {
 
-    public void intercept(HttpRequestBase request) {
-    }
+   public void intercept(HttpRequestBase request) {
+   }
 
 }
--- src/main/java/com/profitbricks/rest/client/RestClientBuilder.java
+++ src/main/java/com/profitbricks/rest/client/RestClientBuilder.java
@@ -10,59 +10,57 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RestClientBuilder {
 
-    protected HttpClient client;
+   protected HttpClient client;
 
-    protected ObjectMapper mapper;
+   protected ObjectMapper mapper;
 
-    protected RequestInterceptor interceptor;
+   protected RequestInterceptor interceptor;
 
-    protected Class<? extends RestClient> clazz;
+   protected Class<? extends RestClient> clazz;
 
-    protected RestClientBuilder() {
-    }
+   protected RestClientBuilder() {
+   }
 
-    public RestClientBuilder httpClient(HttpClient client) {
-	this.client = client;
-	return this;
-    }
+   public RestClientBuilder httpClient(HttpClient client) {
+      this.client = client;
+      return this;
+   }
 
-    public RestClientBuilder objectMapper(ObjectMapper mapper) {
-	this.mapper = mapper;
-	return this;
-    }
+   public RestClientBuilder objectMapper(ObjectMapper mapper) {
+      this.mapper = mapper;
+      return this;
+   }
 
-    public RestClientBuilder requestInterceptor(RequestInterceptor interceptor) {
-	this.interceptor = interceptor;
-	return this;
-    }
+   public RestClientBuilder requestInterceptor(RequestInterceptor interceptor) {
+      this.interceptor = interceptor;
+      return this;
+   }
 
-    public RestClientBuilder restClientClass(Class<? extends RestClient> clazz) {
-	this.clazz = clazz;
-	return this;
-    }
+   public RestClientBuilder restClientClass(Class<? extends RestClient> clazz) {
+      this.clazz = clazz;
+      return this;
+   }
 
-    public RestClient build() {
-	if (clazz == null) {
-	    clazz = RestClient.class;
-	}
-	if (mapper == null) {
-	    mapper = new ObjectMapper();
-	    mapper.setSerializationInclusion(Include.NON_NULL);
-	}
-	if (client == null) {
-	    client = HttpClientBuilder.create().useSystemProperties().build();
-	}
-	return createRestClient(this, clazz);
-    }
+   public RestClient build() {
+      if (clazz == null)
+         clazz = RestClient.class;
+      if (mapper == null) {
+         mapper = new ObjectMapper();
+         mapper.setSerializationInclusion(Include.NON_NULL);
+      }
+      if (client == null)
+         client = HttpClientBuilder.create().useSystemProperties().build();
+      return createRestClient(this, clazz);
+   }
 
-    protected <T extends RestClient> T createRestClient(RestClientBuilder builder, Class<T> restClientClass) {
-	try {
-	    Constructor<T> constructor = restClientClass.getDeclaredConstructor(RestClientBuilder.class);
-	    constructor.setAccessible(true);
-	    return constructor.newInstance(builder);
-	} catch (Exception e) {
-	    throw new RuntimeException(e);
-	}
-    }
+   protected <T extends RestClient> T createRestClient(RestClientBuilder builder, Class<T> restClientClass) {
+      try {
+         Constructor<T> constructor = restClientClass.getDeclaredConstructor(RestClientBuilder.class);
+         constructor.setAccessible(true);
+         return constructor.newInstance(builder);
+      } catch (Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
 
 }
