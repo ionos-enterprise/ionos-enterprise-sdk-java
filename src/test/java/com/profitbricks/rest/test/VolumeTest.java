455cfe57d04ac7c26565dfdff5d22a3f2ac8eea0
jasminSPC <jasmin@stackpointcloud.com>
2015-05-21 14:20:15 +0200

    Added BSD license to all files

--- src/test/java/com/profitbricks/rest/test/VolumeTest.java
+++ src/test/java/com/profitbricks/rest/test/VolumeTest.java
@@ -1,7 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ /*
+ * Copyright (c) <year>, <copyright holder>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the <organization>.
+ * 4. Neither the name of the <organization> nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.profitbricks.rest.test;
 
@@ -33,36 +57,29 @@ public class VolumeTest {
    @BeforeClass
    public static void setUp() throws RestClientException, IOException {
       DataCenter datacenter = new DataCenter();
-
       datacenter.properties.name = "SDK TEST DC";
       datacenter.properties.location = Location.US_LAS;
       datacenter.properties.description = "SDK TEST Description";
 
-      datacenter.entities = null;
-      datacenter.metadata = null;
       DataCenter newDatacenter = profitbricksApi.dataCenterApi.createDataCenter(datacenter);
       dcId = newDatacenter.id;
 
       Server server = new Server();
-
       server.properties.name = "SDK TEST SERVER";
       server.properties.ram = "1024";
       server.properties.cores = "4";
-
-      server.metadata = null;
-      server.entities = null;//.volumes.items.add(volume);
 
       Server newServer = profitbricksApi.serverApi.createServer(dcId, server);
 
       assertNotNull(newServer);
       serverId = newServer.id;
 
-      Volume toCreate = new Volume();
+      Volume volume = new Volume();
+      volume.properties.size = "1024";
+      volume.properties.licenceType = "LINUX";
 
-      toCreate.properties.size = "1024";
-      toCreate.properties.licenceType = "LINUX";
-      Volume newVolume = profitbricksApi.volumeApi.createVolume(dcId, toCreate);
-
+      Volume newVolume = profitbricksApi.volumeApi.createVolume(dcId, volume);
+      assertNotNull(newVolume);
       volumeId = newVolume.id;
    }
 
