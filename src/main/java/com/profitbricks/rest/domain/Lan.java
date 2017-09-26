c3845431290d0b38eea892abf6f6ef423186d529
Denis Besic <besicdenis@gmail.com>
2017-09-27 00:18:10 +0200

    Removing comments

--- src/main/java/com/profitbricks/rest/domain/Lan.java
+++ src/main/java/com/profitbricks/rest/domain/Lan.java
@@ -120,28 +120,6 @@ public class Lan extends ProfitbricksBase {
       }
    }
 
-//   @JsonIgnoreProperties(ignoreUnknown = true)
-//   public static class IpFailover{
-//       private String ip;
-//       private String nicUuid;
-//
-//       public String getIp() {
-//           return ip;
-//       }
-//
-//       public void setIp(String ip) {
-//           this.ip = ip;
-//       }
-//
-//       public String getNicUuid() {
-//           return nicUuid;
-//       }
-//
-//       public void setNicUuid(String nicUuid) {
-//           this.nicUuid = nicUuid;
-//       }
-//   }
-
    private Entities entities = new Entities();
 
    private Properties properties = new Properties();
