aefc73b42dffb7e6eab222c5f092bc769731688e
Maxim Domentii <maxim.domentii@cloud.ionos.com>
2019-07-05 14:28:45 +0300

    Small fix

--- src/main/java/com/ionosenterprise/rest/domain/S3Keys.java
+++ src/main/java/com/ionosenterprise/rest/domain/S3Keys.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class S3Keys extends BaseResource {
 
-    private List<Volume> items = new ArrayList<Volume>();
+    private List<S3Key> items = new ArrayList<S3Key>();
 
     /**
      * @return the items
      */
-    public List<Volume> getItems() {
+    public List<S3Key> getItems() {
         return items;
     }
 
     /**
      * @param items the items to set
      */
-    public void setItems(List<Volume> items) {
+    public void setItems(List<S3Key> items) {
         this.items = items;
     }
 }
